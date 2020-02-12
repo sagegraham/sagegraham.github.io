5931e12f69df9d9886d6c0c3b2ccbd9a3e09c1aa
sagegraham <60900874+sagegraham@users.noreply.github.com>
2020-02-12 15:19:15 -0600

    updated image demo to include mouse wheel data

--- image-demo/sketch.js
+++ image-demo/sketch.js
@@ -6,9 +6,10 @@
 // - describe what you did to take this project "above and beyond"
 
 let fish;
+let scalar = 0.25;
 
 function preload() {
-  fish = loadImage('assets/fish.png');
+  fish = loadImage("assets/fish.png");
 }
 
 function setup() {
@@ -19,5 +20,14 @@ function draw() {
   background(220);
   // fill("red")
   // rect(mouseX,mouseY,75,75);
-  image(fish, mouseX, mouseY);
+  image(fish, mouseX, mouseY, fish.width*scalar, fish.height*scalar);
+}
+
+function mouseWheel(event) {
+  if (event.delta>0) {
+    scalar *= 1.1;
+  }
+  else {
+    scalar *= 0.9;
+  }
 }
