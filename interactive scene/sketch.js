797ce2d540e9e75175b25711786967d1ced83a16
sagegraham <60900874+sagegraham@users.noreply.github.com>
2020-02-12 15:19:49 -0600

    began interactive scene project
    nothing other than name/date

---
+++ interactive scene/sketch.js
@@ -0,0 +1,17 @@
+// Interactive Scene
+// Sage Graham
+// 02/12/20
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
+
+
