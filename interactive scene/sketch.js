448b3c4e5ec0c352753c5f397e6217e1fa54eadd
sagegraham <60900874+sagegraham@users.noreply.github.com>
2020-02-13 13:10:20 -0600

    started setting up the window
    added userColour, colourBox, and easel

--- interactive scene/sketch.js
+++ interactive scene/sketch.js
@@ -1,10 +1,11 @@
 // Interactive Scene
 // Sage Graham
 // 02/12/20
-//
+// 
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let userColour = 0; //user controlled colour
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,6 +13,12 @@ function setup() {
 
 function draw() {
   background(220);
+
+  fill(userColour);
+  let colourBox = rect(50, 50, 100); //box will show current colour
+
+  fill("white"); //creating a canvas for the input
+  let easel = rect(windowWidth/3, windowHeight/6, windowWidth/3, windowHeight/2);
 }
 
 
