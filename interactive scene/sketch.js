d88adce8787c4cec48ab585115a0c6748ced28e4
sagegraham <60900874+sagegraham@users.noreply.github.com>
2020-02-14 15:18:21 -0600

    started keypressed stuff

--- interactive scene/sketch.js
+++ interactive scene/sketch.js
@@ -5,7 +5,11 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let userColour = 0; //user controlled colour
+//user controlled colour values
+let r = 0;
+let g = 0;
+let b = 0;
+let a = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -14,11 +18,28 @@ function setup() {
 function draw() {
   background(220);
 
-  fill(userColour);
+  fill(r, g, b, a);
   let colourBox = rect(50, 50, 100); //box will show current colour
 
   fill("white"); //creating a canvas for the input
   let easel = rect(windowWidth/3, windowHeight/6, windowWidth/3, windowHeight/2);
 }
 
+function keyPressed() {
+  //colour adjustment
+  if (key === "[" && r <= 250) {
+    r+=5;
+  }
+  else if (key === "]" && r >= 5) {
+    r+=5;
+    
+  }
+  if (key === "[" && r <= 250) {
+    r+=5;
+  }
+  if (key === "[" && r <= 250) {
+    r+=5;
+  }
+
+}
 
