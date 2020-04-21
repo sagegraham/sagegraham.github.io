fbb065f2723f4ad04b528f8b84aee0a8590b51d7
sagegraham <60900874+sagegraham@users.noreply.github.com>
2020-04-20 22:19:58 -0600

    lolz when you forget to update your code
    still doesnt work, i'm a disgrace

--- array project/sketch.js
+++ array project/sketch.js
@@ -1,21 +1,21 @@
 // array/state variable project
 // sage graham
 // due april 20/20
-//
+// I am so sorry you had to look at this code
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// - was attempted and then given up on
+// - it has an arrays (around L173) and state variables (pretty much everywhere)...just...doesn't work
 
-//to make: stupid simple catching game; 2d; move across screen to catch falling fruits or sth; different difficulties;
-//session highscore counter; uhhhhh; include html changes for more marks even though it doesnt matter
-
+//so many variables
 let state;
-let fruitArray = [];
+let fruitArray;
 let fruitLimit = 10;
 
 let selectDiff;
 let instructions;
+let difficulty;
+let fruitInt;
 
-// let screen;
 
 let apple;
 let orange;
@@ -23,29 +23,35 @@ let peach;
 let easyButton;
 let normalButton;
 let hardButton;
+let basket;
+let catchCounter;
+let dropCounter;
 
 let easy = {
-  fruit: apples,
-  icon: image(apple, 0, random(windowWidth, windowHeight)),
-  gravity: 0.3,
+  fruit: apple,
+  x: 0,
+  y: random(0, windowWidth),
+  dy: 5,
 };
 let normal = {
-  fruit: oranges,
-  icon: image(orange, 0, random(windowWidth, windowHeight)),
-  gravity: 0.6,
+  fruit: orange,
+  x: 0,
+  y: random(0, windowWidth),
+  dy: 10,
 };
 let hard = {
-  fruit: peaches,
-  icon: image(peach, 0, random(windowWidth, windowHeight)),
-  gravity: 1,
+  fruit: peach,
+  x: 0,
+  y: random(0, windowWidth),
+  dy: 15,
 };
 
 let backButton = {
   x: 0,
   y: 0,
-  width: screen.width/12,
-  height: screen.height/12,
-}
+  width: windowWidth/6,
+  height: windowHeight/6,
+};
 
 function preload() {
   apple = loadImage("apple.png");
@@ -55,16 +61,12 @@ function preload() {
   easyButton = loadImage("easy-icon.png");
   normalButton = loadImage("normal-icon.png");
   hardButton = loadImage("hard-icon.png");
-
 }
 
 function setup() {
-  // let screen = {
-  //   width: windowWidth,
-  //   height: windowHeight,
-  // }
   createCanvas(windowWidth, windowHeight);
   state = "startMenu";
+  fruitArray = [];  
 }
 
 function draw() {
@@ -72,22 +74,29 @@ function draw() {
     startMenu();
   }
   else if (state === "howTo") {
-    console.log("i hate myself");
     showInstructions();
   }
   else if (state === "selectDifficulty") {
     selectGameMode();
   }
   else if (state === "game") {
+    background(104, 204, 191); // this was going to be a cute drawn background but considering i cant see anything its not worth making
+    fruitInterval();
+    addFruit();
     moveFruit();
     displayFruit();
+    userBasket();
     checkCollision();
     displayCounters();
-    }
+    checkDrops();
   }
+  else if (state === "gameover") {
+    gameOver();
+  }
+}
 
+//menus and pre-game stuff
 function startMenu() {
-  console.log("maybe its working");
   selectDiff = { 
     x: windowWidth/3, 
     y: windowHeight/5, 
@@ -103,39 +112,132 @@ function startMenu() {
 
   fill("red");
   rect(selectDiff.x, selectDiff.y, selectDiff.width, selectDiff.height);
+  fill("blue");
   rect(instructions.x, instructions.y, instructions.width, instructions.height);
+  fill("white");
+  text("select difficulty", selectDiff.x, selectDiff.y);
+  text("how to play", instructions.x, instructions.y);
   //add text
   //make it change the colour
   if (mouseIsPressed && mouseX >= selectDiff.x && mouseX <= selectDiff.width && 
   mouseY >= selectDiff.y && mouseY <= selectDiff.height) {
     state = "selectDifficulty";
   }
-  if ((mouseIsPressed) && mouseX >= instructions.x && mouseX <= instructions.width+instructions.x && 
+  if (mouseIsPressed && mouseX >= instructions.x && mouseX <= instructions.width+instructions.x && 
     mouseY >= instructions.y && mouseY <= instructions.height+instructions.y) {
-      state = "howTo";
-      console.log("hewwo");
-      background(255);
-    }
-};
+    state = "howTo";
+  }
+}
 
 function showInstructions() {
-  console.log("istg");
+  background(104, 204, 191);
   textSize(10);
-  fill(50);
+  fill(200);
   textAlign(CENTER, CENTER);
-  text("fruit will fall from the sky! use the arrow keys to move and catch the fruit in your basket! try to catch as many as you can. when "+fruitLimit+" of fruit touch the ground, the game is over.");
-  console.log("pls");
-  // rect(backButton.x, backButton.y, backButton.width, backButton.height);
-  // if (mousePressed && mouseX >= backButton.x && mouseX <= backButton.width &&
-  // mouseY >= backButton.y && mouseY <= backButton.height) {
-  //   state = "startMenu";
-  // };
-};
+  text("fruit will fall from the sky! use the arrow keys to move and catch the fruit in your basket!"+ 
+  " try to catch as many as you can. when "+fruitLimit+" of fruit touch the ground, the game is over.");
+  //was having trouble with text showing up here (when my live server worked) and sent messages to the console before and after
+  //the text was "shown". still not sure what's wrong.
+
+  rect(backButton.x, backButton.y, backButton.width, backButton.height);
+  if (mouseIsPressed && mouseX >= backButton.x && mouseX <= backButton.width &&
+  mouseY >= backButton.y && mouseY <= backButton.height) {
+    state = "startMenu";
+  }
+}
 
 function selectGameMode() {
+  catchCounter = 0;
+  dropCounter = 0;
   background(104, 204, 191);
-  // imageMode(CENTER);
   image(easyButton, windowWidth/2, windowHeight*1/6);
-  image(normalButton, windowwidth/2, windowHeight*1/3);
-  image(hardButton, windowwidth/2, windowHeight*1/2);
+  image(normalButton, windowWidth/2, windowHeight*1/3);
+  image(hardButton, windowWidth/2, windowHeight*1/2);
+  if (mouseIsPressed) {
+    if (mouseY > 0 && mouseY < windowHeight*1/3) {
+      difficulty = "easy";
+    }
+    else if (mouseY > windowHeight*1/3 && mouseY < windowHeight*1/2) {
+      difficulty = "normal";
+    }
+    else if (mouseY > windowHeight*1/2 && mouseY < windowHeight) {
+      difficulty = "hard";
+    }
+  }
+}
+
+function fruitInterval() {
+  fruitInt = window.setInterval(addFruit, 1000);
+}
+
+//actual gameplay (array) stuff
+function displayFruit() {
+  for (let item = 0; item < fruitArray.length; item++) {
+    fruitArray[item].fruit; //feels like i need more to this
+  }
+}
+
+function moveFruit() {
+  for (let item = 0; item < fruitArray.length; item++) {
+    fruitArray[item].y += fruitArray[item].dy;
+  }
+}
+
+function addFruit() {
+  let newFruit;
+  if (difficulty === "easy") {
+    newFruit = image(apple, easy.x, easy.y);
+  }
+  if (difficulty === "normal") {
+    newFruit = image(orange, normal.x, normal.y); 
+  }
+  if (difficulty === "hard") {
+    newFruit = image(peach, hard.x, hard.y);  
+  }
+  fruitArray.push(newFruit);
+}
+
+function userBasket() {
+  basket = rect(mouseX, windowHeight, 25, 25);
+}
+
+function checkCollision() {
+  for (let item = 0; item < fruitArray.length; item++) {
+    //if in basket, adds to caught counter & removes fruit
+    if (fruitArray[item].x >= mouseX - 25/2 && fruitArray[item].x <= mouseX + 25/2 && 
+      fruitArray[item].y > windowHeight - 25) {
+      catchCounter++;
+      fruitArray.splice(item, 1);
+    }
+    //if on ground, removes and adds to drop counter
+    else if (fruitArray[item].y === windowHeight) {
+      dropCounter++;
+      fruitArray.splice(item, 1);
+    }
+  }
+}
+
+function displayCounters() {
+  text("fruit dropped: "+dropCounter, windowWidth - 50, 20);
+  text("fruit caught: "+catchCounter, windowWidth - 50, 50);
+}
+
+function checkDrops() {
+  if (dropCounter === fruitLimit) {
+    state = "gameover";
+    clearInterval(fruitInt); //researched this too much but apparently it still won't work :/
+  }
+}
+
+function gameOver() {
+  text("you dropped 10 fruit, and the game is over. good job! you caught "+catchCounter+" fruit this round.",
+    windowWidth/2, windowHeight/2);
+
+  rect(windowWidth*2/5, windowHeight*3/4, windowWidth/5, windowHeight/8);
+  text("return to main menu", windowWidth/2, windowHeight*3/4);
+
+  if (mouseIsPressed && mouseX >= windowWidth*2/5 && mouseX <= windowWidth*3/5 && 
+    mouseY >= windowHeight*3/4 && mouseY <= windowHeight*7/8) {
+    state = "startMenu";
+  }
 }
